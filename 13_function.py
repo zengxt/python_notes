28990d6410c1478a58fa5dfa26d026c8be475612
zengxt <zengxt1994@163.com>
2021-10-13 23:26:02 +0800

    Python 函数和异常处理

--- 13_function.py
+++ 13_function.py
@@ -4,3 +4,70 @@
 # @Author  : zengxt
 # @File    : 13_function.py
 
+
+# 不可变对象，在函数中改变，实参不会变化，可变对象在函数中改变，实参会随着改变
+def fun(args1, args2):
+    args1 = 100
+    args2.append(args1)
+
+
+num = 10
+lst = [20, 30, 40]
+print('函数调用前，num = ', num)
+print('函数调用前，lst = ', lst)
+
+fun(num, lst)
+
+print('函数调用后，num = ', num)
+print('函数调用后，lst = ', lst)
+
+
+# 返回多个值，是一个元组
+def oddandeven(nums):
+    odd = []
+    even = []
+    for item in nums:
+        if item % 2:
+            odd.append(item)
+        else:
+            even.append(item)
+    return odd, even
+
+
+odd, even = oddandeven(range(10))
+print(odd, even)
+
+
+# 形参默认值
+def defaultvalue(num1, num2=100):
+    print(num1, num2)
+
+
+defaultvalue(200)
+defaultvalue(200, 300)
+
+
+# * 表示个数可变的位置参数, 结果是一个元组
+def params(*args):
+    print('个数可变的位置参数', args)
+
+
+params(10)
+params(10, 20)
+params(10, 20, 30)
+
+
+# ** 表示个数可变的关键字形参，结果是一个字典
+def params(**args):
+    print('个数可变的关键字形参', args)
+
+
+params(args1=10)
+params(args1=10, args2=20)
+params(args1=10, args2=20, args3=20)
+
+
+# 在一个函数中，个数可变的位置形参只能出现一个，个数可变的关键字形参也只能出现一个，
+# 如果同时出现在一个函数中，要求个数可变的位置形参 在 个数可变的关键字形参前面
+def params(*args1, **args2):
+    pass
---
+++ 14_exception.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/10/13 23:09
+# @Author  : zengxt
+# @File    : 14_exception.py
+
+try:
+    num1 = int(input('请输入第一个数字：'))
+    num2 = int(input('请输入第二个数字：'))
+    result = num1 / num2
+    print('结果为', result)
+except ZeroDivisionError:
+    print('除数不能为 0 ')
+except ValueError:
+    print('清输入一个正确的数字。')
+except BaseException as e:
+    print(e)
+else:
+    print('except 和 else 只会执行一个。')
+finally:
+    print('无论是否出现异常，finally里的代码都会执行。')
+print('程序结束！！')
+
+# try except else：异常了执行 except 块，没有异常执行else
+
+# try except else finally：不管异常与否，finally 中的代码都是执行，通常用来释放资源
